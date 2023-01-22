61f9074008536461bb2f6b9afab0bc6c642b856b
ammarhussien <ammar.hussienmo97@gmail.com>
2023-01-22 13:01:28 +0200

    add reviewer ـ finish task

--- src/main/java/example/addReviewer/AddReviewer.java
+++ src/main/java/example/addReviewer/AddReviewer.java
@@ -17,6 +17,44 @@ public class AddReviewer {
     private final By Nationality = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div[6]/div/div[1]/div[2]/div/div[1]/div[1]");
     private final By WorkSide = By.id("workSide");
     private final By WorkEmail = By.id("workEmail");
+    private final By nationalIdNumberIqamaNumber = By.id("nationalIdNumberIqamaNumber");
+    private final By age = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div[6]/div/div[2]/div[1]/div[2]/div/input[2]");
+    private final By mainEmail = By.id("mainEmail");
+    private final By secondEmail = By.id("secondEmail");
+    private final By contactNumber = By.id("contactnumber");
+    private final By academicLevel = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div[6]/div/div[5]/div/div[1]/div[1]/div[1]/div/div[1]");
+    private final By currentJob = By.id("currentJob");
+    private final By qualityExperience = By.id("qualityExperience");
+    private final By trainingCertificate = By.id("trainingCertificate");
+    private final By publicSpecialty = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div[6]/div/div[5]/div/div[2]/div[1]/div[1]/div/div[1]");
+    private final By subSpecialty = By.id("subSpecialty");
+    private final By languages = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div[6]/div/div[5]/div/div[2]/div[1]/div[2]/div/div[1]");
+    private final By delicateSpecialization = By.id("delicateSpecialization");
+    private final By website = By.id("website");
+    private final By uploadFile = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div[6]/div/div[5]/div/div[2]/div[2]/div[3]/div/input");
+    private final By cvSummary = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div[6]/div/div[5]/div/div[3]/div[1]/div/div[2]/div[1]");
+    private final By nextButton_1 = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/ul/li[2]/button");
+    private final By bankName =By.id("bankName");
+    private final By bankAddress = By.id("bankAddress");
+    private final By beneficiaryName = By.id("beneficiaryName");
+    private final By beneficiaryAddress = By.id("beneficiaryAddress");
+    private final By accountNumber = By.id("accountNumber");
+    private final By accountCurrency = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div/div[2]/div[3]/div/div[1]");
+    private final By nextButton_2 = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/ul/li[3]/button");
+    private final By nextButton_3 = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/ul/li[3]/button");
+    private final By nextButton_4 = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/ul/li[3]/button");
+    private final By checkBox = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div/div[1]/div/div/div/div[1]/div[1]/label/input");
+    private final By submitTask = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/ul/li[3]/button");
+    private final By addOtb = By.id("verificationNumberSentEmail");
+    private final By sendTask = By.xpath("/html/body/div[8]/div[2]/div/mat-dialog-container/extra-form-dialog/div/formio/div/div/div/div[2]/div[2]/div[4]/div[1]/div[1]/button");
+    private final By addVisit = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div/div[1]/div[2]/div/button");
+    private final By visitType = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div/ul/li/div[1]/div/div[1]/div[1]/div[1]/div/div[1]");
+    private final By programName = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div/ul/li/div[1]/div/div[1]/div[1]/div[2]/div/div[1]");
+    private final By institutionName = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div/ul/li/div[1]/div/div[1]/div[2]/div[1]/div/div[1]");
+    private final By visitRole = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div/ul/li/div[1]/div/div[1]/div[1]/div[3]/div/div[1]");
+    private final By visitDate = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div/ul/li/div[1]/div/div[1]/div[2]/div[2]/div/input[2]");
+    private final By saveVisit = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div/ul/li/div[1]/div/div[2]/i[1]");
+    private final By visitEvaluation = By.id("visitEvaluation");
     public void setStartService (){
         driver.findElement(StartService).click();
 
@@ -64,10 +102,151 @@ public class AddReviewer {
         final By gender = By.xpath("/html/body/app-root/div/div/div[2]/app-request/app-page/div/div/div/div/div/app-section/div/div/div/div[2]/div/formio/div/div/div/div[6]/div/div[1]/div[1]/div/div/div["+choose+"]/label/input");
         driver.findElement(gender).click();
     }
+    public void setNationalIdNumberIqamaNumber (String number) {
+        driver.findElement(nationalIdNumberIqamaNumber).sendKeys(number);
+    }
     public void setWorkSide(String work) {
         driver.findElement(WorkSide).sendKeys(work);
     }
     public void setWorkEmail(String workEmail) {
         driver.findElement(WorkEmail).sendKeys(workEmail);
     }
+    public void setAge(String ages) {
+        driver.findElement(age).sendKeys(ages);
+    }
+    public void setMainEmail (String email) {
+        driver.findElement(mainEmail).sendKeys(email);
+    }
+    public void setSecondEmail (String email) {
+        driver.findElement(secondEmail).sendKeys(email);
+    }
+    public void setContactNumber (String number) {
+        driver.findElement(contactNumber).sendKeys(number);
+    }
+    public void setAcademicLevel (int choose) {
+        driver.findElement(academicLevel).click();
+        final By scienceGradeItem = By.id("choices--scienceGrade-item-choice-"+choose);
+        driver.findElement(scienceGradeItem).click();
+    }
+    public void setCurrentJob (String job) {
+        driver.findElement(currentJob).sendKeys(job);
+    }
+    public void setQualityExperience (String experience) {
+        driver.findElement(qualityExperience).sendKeys(experience);
+    }
+    public void setTrainingCertificate (String certificate) {
+        driver.findElement(trainingCertificate).sendKeys(certificate);
+    }
+    public void setPublicSpecialty (int specialty) {
+        driver.findElement(publicSpecialty).click();
+        final By publicSpecialtyItem = By.id("choices--publicSpecialty-item-choice-"+specialty);
+        driver.findElement(publicSpecialtyItem).click();
+    }
+    public void setSubSpecialty (String specialty) {
+        driver.findElement(subSpecialty).sendKeys(specialty);
+    }
+
+    public void setLanguages (int language) {
+        driver.findElement(languages).click();
+        final By item = By.id("choices--languages-item-choice-"+ language);
+        driver.findElement(item).click();
+    }
+    public void setDelicateSpecialization (String specialization) {
+        driver.findElement(delicateSpecialization).sendKeys(specialization);
+    }
+    public void setWebsite (String websites) {
+        driver.findElement(website).sendKeys(websites);
+    }
+    /**
+     //     * Provides path of file to the form then clicks upload button
+     //     * @param absolutePathOfFile The complete path of the file to upload
+     //     */
+    public void uploadFile (String absolutePathOfFile){
+        driver.findElement(uploadFile).sendKeys(absolutePathOfFile);
+    }
+    public void setCvSummary (String cv) throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(cvSummary).sendKeys(cv);
+    }
+    public void clickNextOne(){
+        driver.findElement(nextButton_1).click();
+    }
+    public void setBankName( String name ) {
+        driver.findElement(bankName).sendKeys(name);
+    }
+    public void setBankAddress (String address) {
+        driver.findElement(bankAddress).sendKeys(address);
+    }
+    public void setBeneficiaryName (String name) {
+        driver.findElement(beneficiaryName).sendKeys(name);
+    }
+    public void setBeneficiaryAddress (String address) {
+        driver.findElement(beneficiaryAddress).sendKeys(address);
+    }
+    public void setAccountNumber (String accountNum) {
+        driver.findElement(accountNumber).sendKeys(accountNum);
+    }
+    public void setAccountCurrency (int currency) {
+        driver.findElement(accountCurrency).click();
+        final By accountCurrencyItem = By.id("choices--accountCurrency-item-choice-"+currency);
+        driver.findElement(accountCurrencyItem).click();
+    }
+    public void clickNextTwo(){
+        driver.findElement(nextButton_2).click();
+    }
+    public void clickNextThree(){
+        driver.findElement(nextButton_3).click();
+    }
+    public void AddVisitButton() {
+        driver.findElement(addVisit).click();
+    }
+    public void setVisitType (int choose) {
+        driver.findElement(visitType).click();
+        final By visitTypeItem = By.id("choices--visitType-item-choice-" + choose);
+        driver.findElement(visitTypeItem).click();
+    }
+
+    public void setInstitutionName (int choose){
+        driver.findElement(institutionName).click();
+        final By institutionNameItem = By.id("choices--institutionName-item-choice-" + choose);
+        driver.findElement(institutionNameItem).click();
+    }
+    public void setProgramName (int choose) throws InterruptedException {
+        Thread.sleep(3000);
+        driver.findElement(programName).click();
+        final By programNameItem = By.id("choices--programName-item-choice-"+choose);
+        driver.findElement(programNameItem).click();
+    }
+    public void setVisitRole (int choose){
+        driver.findElement(visitRole).click();
+        final By visitRoleItem = By.id("choices--visitRole-item-choice-"+choose);
+        driver.findElement(visitRoleItem).click();
+    }
+    public void setVisitDate (String date){
+        driver.findElement(visitDate).sendKeys(date);
+    }
+    public void setVisitEvaluation (String evaluation) {
+        driver.findElement(visitEvaluation).sendKeys(evaluation);
+    }
+    public void setSaveVisit () {
+        driver.findElement(saveVisit).click();
+    }
+    public void clickNextFour(){
+        driver.findElement(nextButton_4).click();
+    }
+    public void clickCheckBox(){
+        driver.findElement(checkBox).click();
+    }
+    public void setSubmitTask(){
+        driver.findElement(submitTask).click();
+    }
+    public void setAddOtb(String otb) throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(addOtb).sendKeys(otb);
+    }
+    public void setSendTask(){
+        driver.findElement(sendTask).click();
+    }
+
+
 }
--- src/test/java/front/ReviewerRegistration/Local_reviewer_Registration.java
+++ src/test/java/front/ReviewerRegistration/Local_reviewer_Registration.java
@@ -2,7 +2,6 @@ package front.ReviewerRegistration;
 
 import base.MainSetup;
 import example.addReviewer.AddReviewer;
-import example.homePageFront;
 import org.testng.annotations.Test;
 
 public class Local_reviewer_Registration extends MainSetup {
@@ -10,17 +9,57 @@ public class Local_reviewer_Registration extends MainSetup {
     public  void testSuccessfulLogin() throws InterruptedException {
         AddReviewer addReviewer = homePageFront.ChooseAddReviewerTask();
         addReviewer.setStartService();
-        addReviewer.setNameInEnglish("Ammar");
-        addReviewer.setNameInArabic("عمار");
+        addReviewer.setNameInEnglish("Ammar"); //english String only
+        addReviewer.setNameInArabic("عمار"); // arabic String only
         addReviewer.ClickResidencePlace();
         addReviewer.setChooseResidence(1);
         addReviewer.ClickResidenceCity();
         addReviewer.setResidenceCity(1); // from 1 to 43
         addReviewer.setIdentityType(2, 1);
-        // 1 for National Number ,   2 for IqamaNumber if choose iqama must select nationality from 1 to 100
+        //choose 1 for National Number ,   2 for IqamaNumber if choose iqama must select nationality from 1 to 100
+        addReviewer.setNationalIdNumberIqamaNumber("2134123456"); // start with 2 for iqama , 1 for national number
         addReviewer.setGender(1); // 1 for man , 2 for woman because man always number 1 🤣
-        addReviewer.setWorkSide("WorkSide");
-        addReviewer.setWorkEmail("helo@hello.com");
+        addReviewer.setWorkSide("WorkSide"); // arabic or english string
+        addReviewer.setWorkEmail("helo@hello.com"); // email format
+        addReviewer.setAge("20000202");// yyyy-MM-dd
+        addReviewer.setMainEmail("helo@hello.com"); // email format
+        addReviewer.setSecondEmail("helo@hello.com"); // email format
+        addReviewer.setContactNumber("500000005"); // 9 number
+        addReviewer.setAcademicLevel(1); // 1 for first level , 2 for second level
+        addReviewer.setCurrentJob("Current job"); // arabic or english string
+        addReviewer.setQualityExperience("test");// arabic or english string
+        addReviewer.setTrainingCertificate("test");// arabic or english string
+        addReviewer.setPublicSpecialty(1); // from 1 to 20
+        addReviewer.setSubSpecialty("test");// arabic or english string
+        addReviewer.setLanguages(1);
+        // 1 for arabic , 2 for english  to choose arabic and english  addReviewer.setLanguages(1); +  addReviewer.setLanguages(2);
+        addReviewer.setDelicateSpecialization("test");// arabic or english string
+        addReviewer.setWebsite("www.text.com");// arabic or english string
+        addReviewer.uploadFile("/Users/ammarhussien/Downloads/ETEC.pdf");
+        addReviewer.setCvSummary("Hello World");// arabic or english string
+        addReviewer.clickNextOne();
+        addReviewer.setBankName("ts");
+        addReviewer.setBankAddress("test");
+        addReviewer.setBeneficiaryName("test");
+        addReviewer.setBeneficiaryAddress("tesst");
+        addReviewer.setAccountNumber("4386834768734");
+        addReviewer.setAccountCurrency(2);// 1 for USD , 2 for SAR , 3 for EUR
+        addReviewer.clickNextTwo();
+        addReviewer.clickNextThree();
+        addReviewer.AddVisitButton();
+        addReviewer.setVisitType(1); // 1 for programmatic , 2 for institution
+        addReviewer.setInstitutionName(1);
+        addReviewer.setProgramName(1);
+        addReviewer.setVisitDate("20200202");// if choose programmatic only
+        addReviewer.setVisitRole(2); // 1 for team leader , 2 for reviewer, 3 for observed
+        addReviewer.setVisitEvaluation("test");
+        addReviewer.setSaveVisit();
+        addReviewer.clickNextFour();
+        addReviewer.clickCheckBox();
+        addReviewer.setSubmitTask();
+        addReviewer.setAddOtb("123456");
+        //addReviewer.setSendTask();
+
 
     }
 
